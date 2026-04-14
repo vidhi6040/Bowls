59c8ecc3026c42b32c1e181478c3eefe021182c5
Vidhi Subudhi <mailtovidhisubudhi@gmail.com>
2026-04-15 02:17:11 +0530

    Modified the project

--- app/database.py
+++ app/database.py
@@ -4,3 +4,4 @@ client = MongoClient("mongodb://localhost:27017")
 
 db = client["bowls_db"]
 item_master = db["item_master"]
+user_master = db["user_master"]
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from app.database import item_master
+from app.database import item_master, user_master
 
 app = FastAPI()
 
@@ -27,3 +27,29 @@ def breakfast(request: Request, category: str):
             "items": items, 
        }
     )
+    
+@app.get("/register")
+def register_page(request: Request):
+    return templates.TemplateResponse(
+        "register.html", 
+        {
+            "request": request
+        }
+    )
+    
+@app.post("/register")
+def register(name: str = Form(...), email: str = Form(...), password: str = Form(...)):
+        existing_user = users.find_one({"email": email})
+        if existing_user:
+            return {'message': "User already exists"}
+
+    
+@app.get("/login")
+def login_page(request: Request):
+    return templates.TemplateResponse(
+        "login.html", 
+        {
+            "request": request
+        }
+    )
+    
