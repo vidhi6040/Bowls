91b9041142bbac70fc4a0eca3e6d435f8720a4d0
Vidhi Subudhi <mailtovidhisubudhi@gmail.com>
2026-05-13 21:14:39 +0530

    Added docker compose setup

--- app/database.py
+++ app/database.py
@@ -1,10 +1,6 @@
 from pymongo import MongoClient
 
-<<<<<<< HEAD
 #client = MongoClient("mongodb://mongodb:27017")
-=======
-#client = MongoClient("mongodb://localhost:27017")
->>>>>>> 0e843842875e32be52dc497eb06e758b1b0595ab
 #client = MongoClient("mongodb://host.docker.internal:27017")
 client = MongoClient("mongodb://mongodb:27017")
 
